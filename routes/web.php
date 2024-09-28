90706a0b4c769e0396125e56870b22729e1e8ae8
ngochan <21004092@st.vlute.edu.vn>
2024-09-29 01:55:15 +0700

    design_login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\welcome;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [LoginController::class, 'getView'])->name('index-login');
 Route::get('/a',[welcome::class,'a']);
