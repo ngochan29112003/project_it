6e5e7b031ced96d1e8f87683940679ffa7f0acf3
ngochan <21004092@st.vlute.edu.vn>
2024-11-01 18:25:58 +0700

    .

--- resources/views/sinh_vien/lop_hoc_phan/enroll.blade.php
+++ resources/views/sinh_vien/lop_hoc_phan/enroll.blade.php
@@ -8,7 +8,6 @@
                         <div class="d-flex justify-content-between align-items-center mb-3">
                             <h1 class="text-danger fw-bold mb-3 border-bottom pb-2 pt-xl-4">Hệ thống LMS của VLUTE</h1>
                         </div>
-
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb mb-0">
                                 <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
@@ -20,7 +19,6 @@
                 </div>
             </div>
         </header>
-
 
         <div id="page-content" class="row mt-4">
             <div id="region-main-box" class="col-12">
@@ -41,12 +39,14 @@
                                             <div class="text-muted mb-2">Category:
                                                 <a href="#">HỌC KỲ I</a>
                                             </div>
-                                            <div class="enrolmenticons">
+                                            <div class="enrolmenticons mb-3">
                                                 <i class="fas fa-sign-in-alt me-2" title="Self enrolment" aria-label="Self enrolment"></i>
                                             </div>
+                                            <button class="btn btn-success">Đăng ký vào lớp học</button>
                                         </div>
                                     </div>
                                 </div>
+
                                 <div class="text-center py-4">
                                     <form id="coursesearch2" class="d-flex justify-content-center align-items-center" action="#" method="get">
                                         <input type="text" id="coursesearchbox" class="form-control w-50 me-2" name="search" placeholder="Tìm kiếm khóa học...">
