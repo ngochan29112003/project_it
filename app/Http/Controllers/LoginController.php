7a9b8d16c229f0023e94aaaf35adbf7e301b3f5f
ngochan <21004092@st.vlute.edu.vn>
2024-11-03 18:01:10 +0700

    22222222

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,70 +10,70 @@ use function Laravel\Prompts\select;
 
 class LoginController extends Controller
 {
-   public function getViewLogin()
-   {
-       return view('login');
-   }
+    public function getViewLogin()
+    {
+        return view('login');
+    }
 
     public function loginAction(Request $request)
-{
-    // Lấy lại tk và mk được gửi từ Form qua
-    $request->validate([
-        'ten_tai_khoan' => 'required|string',
-        'mat_khau' => 'required|string',
-    ]);
-
-    // Gọi model
-    $account = TaiKhoanModel::where('ten_tai_khoan', $request->ten_tai_khoan)->first();
-
-    // Tài khoản không tồn tại
-    if (!$account) {
-        return response()->json([
-            'success' => false,
-            'status' => 400,
-            'message' => 'Sai tên đăng nhập hoặc mật khẩu',
+    {
+        // Lấy lại tk và mk được gửi từ Form qua
+        $request->validate([
+            'ten_tai_khoan' => 'required|string',
+            'mat_khau' => 'required|string',
         ]);
+
+        // Gọi model
+        $account = TaiKhoanModel::where('ten_tai_khoan', $request->ten_tai_khoan)->first();
+
+        // Tài khoản không tồn tại
+        if (!$account) {
+            return response()->json([
+                'success' => false,
+                'status' => 400,
+                'message' => 'Sai tên đăng nhập hoặc mật khẩu',
+            ]);
+        }
+
+
+        // Kiểm tra password
+        if (!Hash::check($request->mat_khau, $account->mat_khau)) {
+            return response()->json([
+                'success' => false,
+                'status' => 400,
+                'message' => 'Sai tên đăng nhập hoặc mật khẩu'
+            ]);
+        }
+
+        // Lưu MaTK và VaiTro lên session để còn tái sử dụng
+        session(['ma_nguoi_dung' => $account->ma_nguoi_dung]);
+
+
+        $ma_quyen = DB::table('nguoi_dung')
+            ->where('ma_nguoi_dung','=',$account->ma_nguoi_dung)
+            ->select('ma_quyen')
+            ->first();
+
+        // Kiểm tra quyền người dùng và chuyển hướng
+        if ($ma_quyen->ma_quyen == 1) {
+            return response()->json([
+                'success' => true,
+                'status' => 200,
+                'redirect' => route('trang-chu-admin'),
+            ]);
+        } elseif ($ma_quyen->ma_quyen == 2 || $ma_quyen->ma_quyen == 3) {
+            return response()->json([
+                'success' => true,
+                'status' => 200,
+                'redirect' => route('trang-chu'),
+            ]);
+        } else {
+            return response()->json([
+                'success'  => false,
+                'status'   => 400,
+            ]);
+        }
     }
-
-
-    // Kiểm tra password
-    if (!Hash::check($request->mat_khau, $account->mat_khau)) {
-        return response()->json([
-            'success' => false,
-            'status' => 400,
-            'message' => 'Sai tên đăng nhập hoặc mật khẩu'
-        ]);
-    }
-
-    // Lưu MaTK và VaiTro lên session để còn tái sử dụng
-    session(['ma_nguoi_dung' => $account->ma_nguoi_dung]);
-
-
-    $ma_quyen = DB::table('nguoi_dung')
-        ->where('ma_nguoi_dung','=',$account->ma_nguoi_dung)
-        ->select('ma_quyen')
-        ->first();
-
-    // Kiểm tra quyền người dùng và chuyển hướng
-    if ($ma_quyen->ma_quyen == 1) {
-        return response()->json([
-            'success' => true,
-            'status' => 200,
-            'redirect' => route('trang-chu-admin'),
-        ]);
-    } elseif ($ma_quyen->ma_quyen == 2 || $ma_quyen->ma_quyen == 3) {
-        return response()->json([
-            'success' => true,
-            'status' => 200,
-            'redirect' => route('trang-chu'),
-        ]);
-    } else {
-        return response()->json([
-            'success'  => false,
-            'status'   => 400,
-        ]);
-    }
-}
 
 
     public function logoutAction(Request $request)
